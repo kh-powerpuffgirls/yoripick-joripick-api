1c615c62c2f4e08d867481b106c785d3a3a00b52
530hyelim <530hyelim@gmail.com>
2025-08-31 18:59:54 +0900

    initial commit

---
+++ src/main/java/com/kh/ypjp/YoripickJoripickApiApplication.java
@@ -0,0 +1,13 @@
+package com.kh.ypjp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoripickJoripickApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoripickJoripickApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/ypjp/YoripickJoripickApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.ypjp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoripickJoripickApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
