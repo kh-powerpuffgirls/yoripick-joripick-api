f9d6bc33e8de4f136bcf63a85771ae42943aeb72
DESKTOP-RGULR25\user1 <dkgus8551@gmail.com>
2025-09-26 08:53:43 +0900

    mainpage 재료 관리 연결

--- src/main/java/com/kh/ypjp/ingpedia/controller/IngPediaController.java
+++ src/main/java/com/kh/ypjp/ingpedia/controller/IngPediaController.java
@@ -1,6 +1,7 @@
 package com.kh.ypjp.ingpedia.controller;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -14,17 +15,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.kh.ypjp.common.PageInfo;
 import com.kh.ypjp.common.UtilService;
 import com.kh.ypjp.ingpedia.model.dto.IngPediaDto.IngListResponse;
 import com.kh.ypjp.ingpedia.model.dto.IngPediaDto.IngPairResponse;
+import com.kh.ypjp.ingpedia.model.dto.IngPediaDto.IngPediaMainResponse;
 import com.kh.ypjp.ingpedia.model.dto.IngPediaDto.IngPediaPost;
 import com.kh.ypjp.ingpedia.model.dto.IngPediaDto.IngPediaPut;
 import com.kh.ypjp.ingpedia.model.dto.IngPediaDto.IngPediaResponse;
 import com.kh.ypjp.ingpedia.model.dto.IngPediaDto.PagedIngListResponse;
 import com.kh.ypjp.ingpedia.model.service.IngPediaService;
+import com.kh.ypjp.mealplan.model.vo.Food.Recipe;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -123,9 +128,6 @@ public class IngPediaController {
 		if(IngPedia.getPairList().size() > 0) {
 			pairResult2 = ingPediaService.updateIngPair(IngPedia);
 		}
-		System.out.println(methodResult);
-		System.out.println(pairResult1);
-		System.out.println(pairResult2);
 		
 		if(methodResult > 0 && pairResult1 > 0 && pairResult2 > 0 ) {
 			return ResponseEntity.noContent().build();
@@ -154,62 +156,33 @@ public class IngPediaController {
 	
 	
 	
-//
-//	// 상세조회
-//	@GetMapping("/detail/{ingNo}/{userNo}")
-//	public ResponseEntity<MyIngResponse> myingDetail(
-//			@PathVariable long ingNo,
-//			@PathVariable long userNo
-//			){
-//		HashMap param = new HashMap();
-//		param.put("ingNo", ingNo);
-//		param.put("userNo", userNo);
-//		
-//		MyIngResponse myIng = myingService.selectMyIngDetail(param);
-//		
-//		if(myIng == null) {
-//			return ResponseEntity.notFound().build();
-//		} else {
-//			return ResponseEntity.ok().body(myIng);
-//		}
-//	}
-//	
-//	// 수정
-//	@PutMapping("/detail/{ingNo}/{userNo}")
-//	public ResponseEntity<MyIngResponse> updateMying(
-//			@RequestBody MyIngPut mying,
-//			@PathVariable long ingNo,
-//			@PathVariable long userNo
-//			){
-//		mying.setIngNo(ingNo);
-//		mying.setUserNo(userNo);
-//		int result = myingService.updateMying(mying);
-//		
-//		if(result > 0) {
-//			return ResponseEntity.noContent().build();
-//		} else {
-//			return ResponseEntity.notFound().build();
-//			
-//		}
-//	}
-//	
-//	// 메뉴 삭제
-//		@DeleteMapping("/detail/{ingNo}/{userNo}")
-//		public ResponseEntity<Void> deleteMying(
-//				@PathVariable long ingNo,
-//				@PathVariable long userNo
-//				){
-//			HashMap param = new HashMap();
-//			param.put("ingNo", ingNo);
-//			param.put("userNo", userNo);
-//
-//			int result = myingService.deleteMying(param);
-//			
-//			if(result > 0) {
-//				return ResponseEntity.noContent().build();
-//			} else {
-//				return ResponseEntity.notFound().build();
-//				
-//			}
-//		}
+	
+	
+	
+	
+	@GetMapping("/main")
+	public ResponseEntity<List<IngPediaMainResponse>> ingPediaMain(HttpServletRequest request){
+		
+		List<IngPediaMainResponse> response = new ArrayList();
+		response = ingPediaService.selectIngPediaMain();
+		
+			for (IngPediaMainResponse ing : response) {
+				if (ing.getImgUrl() != null && !ing.getImgUrl().isEmpty()) {
+					String imageUrl = ServletUriComponentsBuilder.fromCurrentContextPath()
+							.path("/images/")
+							.path(ing.getImgUrl())
+							.toUriString();
+					
+					ing.setImgUrl(imageUrl);
+				}
+		}
+		
+		
+		if(response == null) {
+			return ResponseEntity.notFound().build();
+		} else {
+			return ResponseEntity.ok().body(response);
+		}
+		
+	}
 }
