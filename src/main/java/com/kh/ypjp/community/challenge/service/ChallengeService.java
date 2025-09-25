c390d63c6bed08fa79e11548fca0a4a53b2ffca4
KHJ702 <k94070e@gmail.com>
2025-09-25 14:26:41 +0900

    0925 백엔드 클래스 탈퇴

--- src/main/java/com/kh/ypjp/community/challenge/service/ChallengeService.java
+++ src/main/java/com/kh/ypjp/community/challenge/service/ChallengeService.java
@@ -153,11 +153,9 @@ public class ChallengeService {
 
         for (ChallengeReplyDto reply : replies) {
             if (reply.getProfileImageServerName() != null && !reply.getProfileImageServerName().isEmpty()) {
-                // 이 부분이 수정된 로직입니다.
-                // profile/{userno}/{파일명.png} 형식으로 경로를 완성합니다.
+
                 String fullPath = "profile/" + reply.getUserNo() + "/" + reply.getProfileImageServerName();
-                
-                // WebConfig에서 /images/로 매핑했으므로, images/를 붙여 최종 URL을 만듭니다.
+
                 String imageUrl = "/images/" + fullPath;
                 reply.setProfileImageServerName(imageUrl);
             }
