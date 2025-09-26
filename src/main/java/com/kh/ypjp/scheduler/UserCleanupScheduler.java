f94073df4aaf3e0927dd189d18d33284ddca0f16
DESKTOP-S6H8PKS\user1 <hanel9900906@gmail.com>
2025-09-26 17:55:30 +0900

    밥안먹어

---
+++ src/main/java/com/kh/ypjp/scheduler/UserCleanupScheduler.java
@@ -0,0 +1,24 @@
+package com.kh.ypjp.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kh.ypjp.user.dao.UserDao;
+
+@Component
+public class UserCleanupScheduler {
+
+    private final UserDao userDao;
+
+    public UserCleanupScheduler(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
+    @Transactional
+    public void deleteInactiveUsers() {
+        int deleted = userDao.deleteInactiveUsersOver90Days();
+        System.out.println("삭제된 비활성 유저 수: " + deleted);
+    }
+}
