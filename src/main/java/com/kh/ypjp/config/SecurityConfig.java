6b3f304a6eb07156225c80b7097fc76c2e1cc491
530hyelim <530hyelim@gmail.com>
2025-10-05 00:24:49 +0900

    no message

--- src/main/java/com/kh/ypjp/config/SecurityConfig.java
+++ src/main/java/com/kh/ypjp/config/SecurityConfig.java
@@ -99,7 +99,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://43.203.124.138:80"));
+        config.setAllowedOrigins(List.of("http://43.203.124.138"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Location", "Authorization"));
